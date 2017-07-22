b8d72024bbab50b234f387e55e7b46b312d12dd9
Noah Yarborough <ndyarborough1997@gmail.com>
2017-07-22 19:26:24 -0400

    full question set added along with gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,18 +3,38 @@ questions[0] = "What does BMW stand for?";
 questions[1] = "With Tesla's new 'Ludicrous Mode', the Model S P100D can reach 60MPH from a dead stop in __?"
 questions[2] = "Reaching speeds of 420 km/h, how many turbochargers does the new Buggati Chiron have?"
 questions[3] = "What car maker produced the first car to acheive a perfect weight (kg) to power (hp) ratio of 1:1?"
+questions[4] = "Which american muscle icon had a roof so tough, that it broke the crushing machine in the roof crush test?";
+questions[5] = "Formula One car's exhaust can reach temperatures of _____?";
+questions[6] = "The average Formula 1 pit stop is less than _____ seconds.";
+questions[7] = "The most stolen car in the United States is the _____.";
+questions[8] = "In 2008, a 1961 Ferrari California Spyder became the most expensive car sold at a vintage car auction for _____ after the deduction of the auction fees.";
+questions[9] = "The fastest known speeding ticket was issued in May 2003 in Texas. The motorist was operating a Koenigseggs CCR, a Swedish sports car and was allegedly going ___.";
 
 var choices = [];
 choices[0] = ["Berlin Motor Works", "Bavarian Motor Works", "Borgholzhausen Motor Works", "Brunswick Motor Works"];
 choices[1] = ["1.5 seconds", "2.3 seconds", "3.0 seconds", "4.2 seconds"];
 choices[2] = ["0", "4", "2", "8"];
 choices[3] = ["Lamborghini", "Bugatti", "Pagani", "Koenigsegg"];
+choices[4] = ["Dodge Charger", "Ford GT", "Chevrolet Camaro", "Plymouth Road Runner"];
+choices[5] = ["200 degrees celcius", "400 degrees celcius", "800 degrees celcius", "1000 degrees celcius"];
+choices[6] = ["3", "5", "8", "10"];
+choices[7] = ["Toyota Camry", "Ford Fusion", "Honda Accord", "Mercedes Benz E350"];
+choices[8] = ["$10.9 Million USD", "$4.5 million USD", "25.6 Million USD", "17.3 Million USD"];
+choices[9] = ["162 MPH", "197 MPH", "242 MPH", "259 MPH"];
+
 
 var answers = [];
 answers[0] = "Bavarian Motor Works";
 answers[1] = "2.3 seconds"; 
 answers[2] = "4"; 
 answers[3] = "Koenigsegg"; 
+answers[4] = "Ford GT"; 
+answers[5] = "1000 degrees celcius"; 
+answers[6] = "3"; 
+answers[7] = "Honda Accord"; 
+answers[8] = "$10.9 Million USD"; 
+answers[9] = "242 MPH"; 
+
 
 var timeRemaining;
 var questionCounter = 0;
@@ -29,7 +49,6 @@ var intervalID;
 function start() {
 	$(".start").remove()
 	$("<div class='live-box'></div>").insertAfter($(".main-header"));
-	$(".live-box").append($("<h1 class='timeRemaining'>Time Remaining: <span id='timeRemaining'>30</span> seconds</h1>"));
 	$(".live-box").append($("<h1 class='question'></h1>"));
 	$(".live-box").append($("<div class='choices'></div>"));
 	nextQuestion();
@@ -38,6 +57,7 @@ function start() {
 function thirtySecTimer() {
 
 		timeRemaining = 30;
+    $(".live-box").prepend($("<h1 class='timeRemaining'>Time Remaining: <span id='timeRemaining'>30</span> seconds</h1>"));
 		$("#timeRemaining").text(timeRemaining);
 		intervalID = setInterval(function(){ 
 		
@@ -57,6 +77,7 @@ function thirtySecTimer() {
 function nextQuestion() {
 	$(".gifHead").remove();
 	$(".giffy").remove();
+  $(".timeRemaining").remove();
 	clearInterval(intervalID);
 
 	thirtySecTimer();
@@ -82,10 +103,12 @@ function transition() {
 	setTimeout(nextQuestion, 4000);
 	if(userGuess === answers[questionCounter - 1]){
 	$(".live-box").append($("<h1 class='gifHead'>Correct!</h1>"));
+	$(".live-box").append($("<img height='300px' class='giffy' src='assets/images/" + (questionCounter -1) + ".gif'>"));
 	} else {
 		$(".live-box").append($("<h1 class='gifHead'>Wrong Answer! It was " + answers[questionCounter - 1] + "</h1>"));
+		$(".live-box").append($("<img height='300px' class='giffy' src='assets/images/wrong.gif'>"));
 	}
-	$(".live-box").append($("<img class='giffy' src='assets/images/" + (questionCounter -1) + ".gif'>"));	
+		
 	$(".timeRemaining").empty();
 	$(".question").empty();
 	$(".choices").empty();
